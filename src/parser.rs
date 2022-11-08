27bfdae8b0546112752eef97541af1b3fe6aff62
Arijit Basu <sayanarijit@gmail.com>
2022-11-08 19:27:23 +0530

    Do it in style

--- src/parser.rs
+++ src/parser.rs
@@ -1,40 +1,38 @@
 use crate::token::Token;
-use nom::bytes::complete::take;
+use nom::branch::alt;
 use nom::bytes::complete::take_while1;
+use nom::character::complete::char;
 use nom::character::is_alphanumeric;
+use nom::combinator::map;
+use nom::multi::fold_many1;
+use nom::sequence::preceded;
 use nom::IResult;
 
-fn constant(i: &[u8]) -> IResult<&[u8], Token> {
-    let (i, res) = take_while1(|c| c != b'$')(i)?;
-    Ok((i, Token::Const(res)))
+fn parse_constant(i: &[u8]) -> IResult<&[u8], Token> {
+    map(take_while1(|c| c != b'$'), Token::Const)(i)
 }
 
-fn variable(i: &[u8]) -> IResult<&[u8], Token> {
-    let (i, s) = take(1usize)(i)?;
-    if i.len() == 0 {
-        return Ok((i, Token::Const(s)));
-    }
-
-    let (i, res) = take_while1(|c| c == b'_' || is_alphanumeric(c))(i)?;
-    Ok((i, Token::Var(res)))
+fn parse_variable_body(i: &[u8]) -> IResult<&[u8], Token> {
+    map(take_while1(|c| c == b'_' || is_alphanumeric(c)), Token::Var)(i)
 }
 
-pub(crate) fn parse(mut i: &[u8]) -> IResult<&[u8], Vec<Token>> {
-    let mut tokens = vec![];
+fn parse_dollar(i: &[u8]) -> IResult<&[u8], Token> {
+    map(char('$'), Token::Char)(i)
+}
 
-    while !i.is_empty() {
-        let fun = if i.first() == Some(&b'$') {
-            variable
-        } else {
-            constant
-        };
+fn parse_variable(i: &[u8]) -> IResult<&[u8], Token> {
+    preceded(char('$'), parse_variable_body)(i)
+}
 
-        let (i_, tok) = fun(i)?;
+pub(crate) fn parse_fragment(i: &[u8]) -> IResult<&[u8], Token> {
+    alt((parse_variable, parse_constant, parse_dollar))(i)
+}
+
+pub(crate) fn parse(i: &[u8]) -> IResult<&[u8], Vec<Token>> {
+    fold_many1(parse_fragment, Vec::new, |mut tokens, tok| {
         tokens.push(tok);
-        i = i_;
-    }
-
-    Ok((i, tokens))
+        tokens
+    })(i)
 }
 
 #[cfg(test)]
@@ -43,12 +41,26 @@ mod tests {
 
     #[test]
     fn test_parse_constant() {
-        assert_eq!(parse(b"foo.bar").unwrap().1, vec![Token::Const(b"foo.bar")]);
+        assert_eq!(
+            parse_constant(b"foo.bar").unwrap().1,
+            Token::Const(b"foo.bar")
+        );
     }
 
     #[test]
     fn test_parse_variable() {
-        assert_eq!(parse(b"$foo").unwrap().1, vec![Token::Var(b"foo")]);
+        assert_eq!(parse_variable(b"$foo").unwrap().1, Token::Var(b"foo"));
+    }
+
+    #[test]
+    fn test_dollar() {
+        assert_eq!(parse_dollar(b"$").unwrap().1, Token::Char('$'));
+    }
+
+    #[test]
+    fn test_parse_fragment() {
+        assert_eq!(parse_fragment(b"foo").unwrap().1, Token::Const(b"foo"));
+        assert_eq!(parse_fragment(b"$foo").unwrap().1, Token::Var(b"foo"));
     }
 
     #[test]
@@ -60,7 +72,7 @@ mod tests {
                 Token::Var(b"bar"),
                 Token::Const(b".foo."),
                 Token::Var(b"bar"),
-                Token::Const(b"$"),
+                Token::Char('$'),
             ]
         );
     }
--- src/token.rs
+++ src/token.rs
@@ -4,6 +4,7 @@ use std::string::FromUtf8Error;
 pub(crate) enum Token<'a> {
     Const(&'a [u8]),
     Var(&'a [u8]),
+    Char(char),
 }
 
 impl Token<'_> {
@@ -11,6 +12,7 @@ impl Token<'_> {
         match self {
             Token::Const(s) => String::from_utf8(s.into()),
             Token::Var(s) => String::from_utf8(s.into()),
+            Token::Char(c) => Ok(c.into()),
         }
     }
 }
